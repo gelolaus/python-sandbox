7f7afd8d70b85f7efde4c28a6a2c4c407ab4943e
Angelo Laus <angelolausofcl@gmail.com>
2023-05-06 14:09:17 +0800

    feat: add Lecture 1 programs

---
+++ CS50P/Lectures/Lecture 1/compare.py
@@ -0,0 +1,25 @@
+# Ask for the value of x and y
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+"""
+# If x is less than y
+if x < y:
+    print("x is less than y")
+
+# Else if x is greater than y
+elif x > y:
+    print("x is greater than y")
+
+# Else x is equal to y
+else:
+    print("x is equal to y")
+
+"""
+# If x is equal to y print "equal to"
+if x == y:
+    print("x is equal to y")
+
+# If x is less than or equal to y print "not equal to"
+else:
+    print("x is not equal to y")
